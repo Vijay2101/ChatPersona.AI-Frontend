6955c74531065e80b5f3eca1915a7111eee49ff8
Vijay Kumar <kumarvijay2003.vk@gmail.com>
2024-12-20 13:37:24 +0530

    Add files via upload

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
-
+import { Link } from 'react-router-dom';
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
